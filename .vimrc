a69430010f4a21d20de11807c621eb1cfad852a6
Chamsidine ATTOUMANI <attouchams@gmail.com>
2015-12-05 16:49:00 +0100

    updaded vimrc

--- .vimrc
+++ .vimrc
@@ -1,10 +1,12 @@
+syntax on
+set nu
 "indent with tab for c file
-set tabstop=4 
+set tabstop=4
 set shiftwidth=4
-set autoindent 
+set autoindent
 set cindent
 
-"
+" close pairs
 inoremap {      {}<Left>
 inoremap {<CR>  {<CR>}<Esc>O
 inoremap {{     {
@@ -15,7 +17,36 @@ inoremap (<CR>  (<CR>)<Esc>O
 inoremap ((     (
 inoremap ()     ()
 
-"Enable match highlighting when starting vim
-set viminfo^=h
+inoremap [      []<Left>
+inoremap [<CR>  [<CR>]<Esc>O
+inoremap [[     ]
+inoremap []     []
 
-inoremap <Space>		<Space><Esc>/\s\s<CR>i
+inoremap '      ''<Left>
+inoremap '<CR>  '<CR>'<Esc>O
+inoremap ''     '
+inoremap ''     ''
+
+inoremap "      ""<Left>
+inoremap "<CR>  "<CR>"<Esc>O
+inoremap ""     "
+inoremap ""     ""
+
+" display cursor column
+set statusline+=col:\ %c
+
+" config spaces side by side
+highlight DoubleSpaces ctermbg=green guibg=red
+autocmd BufWinEnter * match ExtraWhitespace /\s\s/
+autocmd InsertLeave * match ExtraWhitespace /\s\s/
+autocmd BufWinLeave * call clearmatches()
+
+highlight ExtraWhitespace ctermbg=red guibg=red
+autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+autocmd BufWinLeave * call clearmatches()
+
+"toggle on and off hlsearch
+nnoremap <F4> :set hlsearch! hlsearch?<CR>
+inoremap <F4> <Esc>:set hlsearch! hlsearch?<CR>i
---
+++ t.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t.c                                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cattouma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/05 16:05:30 by cattouma          #+#    #+#             */
+/*   Updated: 2015/12/05 16:44:39 by cattouma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+fdsf   kjfkdjsfkdjs  fdskfsdkfjsd  fdskfjdkajfa  fdakfjdkajfa  fdsakfjda;fjd;akj  fdka;fjd;kajfa  fdsakfjd;ksajfds  fdsk;afjdksjfsd;f f sdkfj;ajf;sak 
+
+
+  ifffffffdfdfdfdffdsfsdfdsfdsfd fjdskfjskfjsdkfjdksfjdfdsfdsfdsfdsfdsfdsfdfdf   fdksfjdksf  fdskfjdsk fdksfjdskj fdskfjsdk  fdskjfsdkjfsj jfds	ssdf <F4>i  <F4>  jkfjdskf  fdsfdsf  fdskfjdskfjds  fdskfjdksfjsd  fdskfjdskjfdskf  fdskfjdskjfds;k fdskajf;jk  <F4><F4><F4>
+
+
+"  fdsfdsf kjfdksjf     fdksfkdsfjds  f    fdsjkk   kjdkjfdskfdsf fjdskfjdskfjds fjdksfjdskfjdkfdsjfkdsjfsdkjfsdkfjdsk ffjdskfjksdjfdskjfsdfsfjfdsfdsfdfdfdsfs /   fdskjsd  jkj fsfdsfds   fdsfdsf  fdksfdsk  kdsjfdskjfsf     :sfdsfdsfdsfdsf"   kfdjkf    fdskfjdsk 
+fsdfdsfs 
+ fsdfdsfds 
+  fsfdsfdsfds 
+  fdsfdsfs    
+   fsfdsfdsfds     
+
